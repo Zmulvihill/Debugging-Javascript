f54b0484423234c3a77778999223f3301d0041ac
Zmulvihill <zmulvihi@yahoo.com>
2023-02-18 13:23:27 -0700

    added script2 and ran node

---
+++ script2.js
@@ -0,0 +1,9 @@
+//Understanding the Differences between the freeCodeCamp and Browser Console
+// Open your browser console.
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+// Use console.log() to print the output variable.
+console.log(output);
+
+// Use console.clear() to clear the browser console.
+console.clear();
